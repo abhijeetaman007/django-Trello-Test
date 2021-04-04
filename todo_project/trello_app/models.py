3a56b5399ac4c9884390ab30bd1d38f360bcadb0
Abhijeet Sinha <abhijeetaman007@gmail.com>
2021-04-04 16:42:00 +0530

    Models changed

--- todo_project/trello_app/models.py
+++ todo_project/trello_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 
 
@@ -27,11 +26,27 @@ from django.utils import timezone
 
 
 class TaskList(models.Model):
+    def __str__ (self):
+        return self.name
     name = models.TextField(max_length=50)
-    created_at = models.DateTimeField(default = timezone.now)
+    # created_at = models.DateTimeField(default = timezone.now)
+    created_at = models.DateTimeField(default = timezone.localtime)
+    
+
     
 class Task(models.Model):
+    def __str__(self):
+        return f"{self.content}-{self.task_list}"
     content = models.TextField()
-    created_at = models.DateTimeField(default= timezone.now)
+    created_at = models.DateTimeField(default = timezone.now)
+    # created_at = models.DateTimeField(default= timezone.localtime())
     deadline = models.DateTimeField(blank=True , null= True)
     task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('P','PENDING'),
+        ('C','COMPLETE'),
+        ('IP','IN_PROGRESS'),
+        ('D','DROPPED')
+    )
+    status = models.CharField(choices=STATUS_CHOICES,default=STATUS_CHOICES[2],max_length=2)
+    
