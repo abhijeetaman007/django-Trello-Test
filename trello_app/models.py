63b173de14b87b4f6f2eeb6589255742fd1bc887
Abhijeet Sinha <abhijeetaman007@gmail.com>
2021-04-04 10:37:45 +0530

    For testing

---
+++ trello_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+#Requirements
+    # -Tasks 
+    #     -Content 
+    #     -creation date
+    #     -deadline
+    #     - TaskList  
+
+    #     -status
+    #     -completed_on    
+    #
+    #  -TaskList
+            # -name
+# Tables
+#     * Task Tables
+#     id | content | creation_table | deadline | task_list_id | completed_on 
+#     * Task List
+#         (like all task related to college in one)
+#         id | Name | created_at
+
+
+
+class TaskList(models.Model):
+    name = models.TextField(max_length=50)
+    created_at = models.DateTimeField(default = timezone.now)
+    
+class Task(model.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(default= timezone.now)
+    deadline = models.DateTimeField(blank=True , null= True)
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
